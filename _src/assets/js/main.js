676f828436499f960fc892de302cbde120ed1d79
elviac94 <elviac94@gmail.com>
2020-03-19 18:57:38 +0100

    add default image

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -12,26 +12,34 @@ let inputElem = null;
 const urlBase =  "http://api.tvmaze.com/search/shows?q=";
 
 function buttonHandler(){
-    inputElem = inputSearchSeries.value;
     getSeries();
+
 }
 
 
 function getSeries() {
+    inputElem = inputSearchSeries.value;
     fetch(`${urlBase}${inputElem}`)
       .then(response => response.json())
       .then(data => {
         series = data;
         paintResult(series);
+        
       });
   }
 
   function paintResult(arr) {
     for (let item of arr) {
 
-     consultedSeries.innerHTML += `<li class="list-item" id=${item.show.id}><p class="main-title">${item.show.name}</p><img src=${item.show.image.medium}></li>`;
+     consultedSeries.innerHTML = `<li class="list-item" id=${item.show.id}><p class="main-title">${item.show.name}</p><img src=${item.show.image.medium}></li>`;
 
+    }
+
+     if (item.show.image === null) {
+
+        item.show.image.medium = "https://via.placeholder.com/210x295";
+            
   }
   }
-  
+
 searchButton.addEventListener('click', buttonHandler);
